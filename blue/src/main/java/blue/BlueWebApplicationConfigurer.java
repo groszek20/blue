a2548d577edf8a0c22e658cee4ffa80cd78191ca
groszek20 <pea@op.pl>
2018-03-05 21:03:26 +0100

    configuration classes

---
+++ blue/src/main/java/blue/BlueWebApplicationConfigurer.java
@@ -0,0 +1,22 @@
+package blue;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class BlueWebApplicationConfigurer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {BlueWebConfigurer.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
---
+++ blue/src/main/java/blue/BlueWebConfigurer.java
@@ -0,0 +1,10 @@
+package blue;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("blue")
+public class BlueWebConfigurer {
+
+}
